bc8fe9b57ac7ce9aff2d82f5c589a39fab843fcf
Dilgo-dev <dilgopierre@gmail.com>
2025-05-24 19:29:22 +0200

    feat: Add room number notification and client count update in WebSocket server

--- internal/handlers/websocket.go
+++ internal/handlers/websocket.go
@@ -62,6 +62,7 @@ func (s *server) joinRoom(ws *websocket.Conn, roomId string) {
 		s.rooms[roomId] = make(map[*websocket.Conn]bool)
 	}
 	s.rooms[roomId][ws] = true
+	s.sendRoomNumber(ws, roomId)
 }
 
 func (s *server) leaveRoom(ws *websocket.Conn, roomId string) {
@@ -71,4 +72,9 @@ func (s *server) leaveRoom(ws *websocket.Conn, roomId string) {
 		delete(s.rooms, roomId)
 		fmt.Printf("Room %s is empty, removing it 🐡\n", roomId)
 	}
+}
+
+func (s *server) sendRoomNumber(ws *websocket.Conn, roomId string) {
+	ws.Write([]byte(fmt.Sprintf("Room %s has %d clients 🐡", roomId, len(s.rooms[roomId])-1)))
+	s.broadcastToRoom(roomId, fmt.Sprintf("New client joined the room %s 🐡", roomId))
 }
