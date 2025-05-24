7c258f130429eed3315165a5e9a407adf8908bee
Dilgo-dev <dilgopierre@gmail.com>
2025-05-24 18:14:12 +0200

    feat: Load environment variables from .env file and configure server port dynamically

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,17 +1,26 @@
 package main
 
 import (
+	"log"
 	"net/http"
+	"os"
 
+	"github.com/joho/godotenv"
 	"github.com/labstack/echo/v4"
 )
 
 func main() {
+	if err := godotenv.Load(); err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	port := os.Getenv("PORT")
+
 	e := echo.New()
 
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello, Routine Chat ! 🎣")
 	})
 
-	e.Logger.Fatal(e.Start(":3333"))
+	e.Logger.Fatal(e.Start(":" + port))
 }
