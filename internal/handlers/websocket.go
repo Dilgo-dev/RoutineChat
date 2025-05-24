aa2b09f7ed007984bde63684c56949cff541b379
Dilgo-dev <dilgopierre@gmail.com>
2025-05-24 18:44:55 +0200

    feat: Implement WebSocket server to handle client connections and messages

---
+++ internal/handlers/websocket.go
@@ -0,0 +1,40 @@
+package handlers
+
+import (
+	"fmt"
+	"io"
+
+	"golang.org/x/net/websocket"
+)
+
+type server struct {
+	clients map[*websocket.Conn]bool
+}
+
+func NewServer() *server {
+	return &server{
+		clients: make(map[*websocket.Conn]bool),
+	}
+}
+
+func (s *server) HandleWS(ws *websocket.Conn) {
+	s.clients[ws] = true
+
+	fmt.Println("New client connected:", ws.RemoteAddr())
+
+	for {
+		msg := make([]byte, 512)
+		if _, err := ws.Read(msg); err != nil {
+			if err == io.EOF {
+				fmt.Println("Client disconnected")
+				break
+			}
+			fmt.Println("Error reading message", err)
+			continue
+		}
+
+		fmt.Println("Message received:", string(msg))
+		ws.Write([]byte("Message received from server 🎣"))
+	}
+
+}
